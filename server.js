ae6529f2350cf6fe200251bf731397ee81232dd8
jatasingh128 <45856187+jatasingh128@users.noreply.github.com>
2021-09-26 13:22:05 +0530

    Changed from web

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var express= require("express");
 var app= express();
 
 app.get('/',(req,res)=>{
-    console.log("welcome to node js");
+    console.log("welcome to node js edited code from web");
 })
 app.listen(8000);
